42396eac9ff1d362c0dd75b312a197359ad04512
XieJiyue <2428207444@qq.com>
2024-09-26 17:18:24 +0800

    前端实现的功能： 1. 根据文件类型显示不同的图标，支持：目录、压缩包、视频、音频、文本、未知类型。    识别文件类型时，使用文件扩展名和文件类型的映射表（在frontend/static/js/grlobal.js里面） 2. 以可读性较好的方式显示文件（不包括目录）在磁盘上的占用空间，即文件大小 3. 在前端中加入排序按钮，可以自定义排序，提供的排序方式有以下几种：    (1) 根据文件类型进行排序（默认的排序方式），顺序为：目录>压缩包>视频>音频>文本>未知；也提供了逆序。        无论是顺序还是逆序，对于同一类型的文件，都按照文件名顺序进行排序    (2) 根据文件名进行排序，顺序为ASCII码从小到大，逆序为ASCII码从大到小    (3) 根据文件大小进行排序，顺序为从小到大，逆序为从大到小。        需要注意的是，因为目录不提供大小，所以在这种排序方式中，目录总是排在前面，并且目录本身会按照文件名顺序进行排序

--- frontend/static/js/global.js
+++ frontend/static/js/global.js
@@ -1,3 +1,86 @@
+window.extensionTypeMap = {
+    'rar': 'package',
+    'zip': 'package',
+    '7z': 'package',
+    'gz': 'package',
+    'tar': 'package',
+    'mp4': 'video',
+    'm4v': 'video',
+    'mkv': 'video',
+    'webm': 'video',
+    'mov': 'video',
+    'avi': 'video',
+    'wmv': 'video',
+    'mpg': 'video',
+    'flv': 'video',
+    'mpeg': 'video',
+    'rm': 'video',
+    'ram': 'video',
+    'rmvb': 'video',
+    'jpg': 'image',
+    'png': 'image',
+    'jpeg': 'image',
+    'gif': 'image',
+    'webp': 'image',
+    'ico': 'image',
+    'bmp': 'image',
+    'psd': 'image',
+    'dwg': 'image',
+    'xcf': 'image',
+    'jpx': 'image',
+    'apng': 'image',
+    'cr2': 'image',
+    'tif': 'image',
+    'jxr': 'image',
+    'heic': 'image',
+    'mp3': 'audio',
+    'wav': 'audio',
+    'm4a': 'audio',
+    'flac': 'audio',
+    'aac': 'audio',
+    'ogg': 'audio',
+    'mid': 'audio',
+    'amr': 'audio',
+    'aiff': 'audio',
+    'txt': 'text',
+    'py': 'text',
+    'js': 'text',
+    'ipynb': 'text',
+    'ini': 'text',
+    'css': 'text',
+    'scss': 'text',
+    'sass': 'text',
+    'html': 'text',
+    'xml': 'text',
+    'json': 'text',
+    'java': 'text',
+    'c': 'text',
+    'cpp': 'text',
+    'md': 'text'
+}
+
+window.fileTypeIconMap = {
+    'dir': `<svg xmlns="http://www.w3.org/2000/svg" width="146.43" height="128" viewBox="0 0 1025 896"><path d="M960.232 896h-896q-26 0-45-19t-19-45V256h1024v576q0 26-19 45t-45 19m-960-704V64q0-27 18.5-45.5T64.232 0h384q26 0 45 18.5t19 45.5t18.5 45.5t45.5 18.5h384q26 0 45 19t19 45z"/></svg>`,
+    'package': `<svg xmlns="http://www.w3.org/2000/svg" width="128.13" height="128" viewBox="0 0 1025 1024"><path d="M896.428 1024h-768q-53 0-90.5-37.5T.428 896V128q0-53 37.5-90.5t90.5-37.5h320v64h-32q-13 0-22.5 9.5t-9.5 22.5t9.5 22.5t22.5 9.5h32v64h-32q-13 0-22.5 9.5t-9.5 22.5t9.5 22.5t22.5 9.5h32v64h-32q-13 0-22.5 9.5t-9.5 22.5t9.5 22.5t22.5 9.5h32v64h-32q-13 0-22.5 9.5t-9.5 22.5t9.5 22.5t22.5 9.5h32v64q-27 0-45.5 19t-18.5 45v192q0 27 18.5 45.5t45.5 18.5h128q27 0 45.5-18.5t18.5-45.5V640q0-26-18.5-45t-45.5-19h32q13 0 22.5-9.5t9.5-22.5t-9.5-22.5t-22.5-9.5h-32v-64h32q13 0 22.5-9.5t9.5-22.5t-9.5-22.5t-22.5-9.5h-32v-64h32q13 0 22.5-9.5t9.5-22.5t-9.5-22.5t-22.5-9.5h-32v-64h32q13 0 22.5-9.5t9.5-22.5t-9.5-22.5t-22.5-9.5h-32V64h32q13 0 22.5-9.5t9.5-22.5t-9.5-22.5t-22.5-9.5h288q53 0 90.5 37.5t37.5 90.5v768q0 53-37.5 90.5t-90.5 37.5m-384-192q-26 0-45-18.5t-19-45t18.5-45.5t45.5-19t45.5 19t18.5 45.5t-19 45t-45 18.5"/></svg>`,
+    'video': `<svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 48 48"><defs><mask id="IconifyId1922cdc16e4b745fe8"><g fill="none" stroke="#fff" stroke-linejoin="round" stroke-width="4"><path fill="#555" d="M4 10a2 2 0 0 1 2-2h36a2 2 0 0 1 2 2v28a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2z"/><path stroke-linecap="round" d="M36 8v32M12 8v32m26-22h6m-6 12h6M4 18h6m-6-2v4M9 8h6M9 40h6M33 8h6m-6 32h6M4 30h6m-6-2v4m40-4v4m0-16v4"/><path fill="#555" d="m21 19l8 5l-8 5z"/></g></mask></defs><path d="M0 0h48v48H0z" mask="url(#IconifyId1922cdc16e4b745fe8)"/></svg>`,
+    'audio': `<svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 24 24"><path d="M16 9h-3v5.5a2.5 2.5 0 0 1-2.5 2.5A2.5 2.5 0 0 1 8 14.5a2.5 2.5 0 0 1 2.5-2.5c.57 0 1.08.19 1.5.5V7h4m3-4H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2"/></svg>`,
+    'image': `<svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 32 32"><path d="M19 14a3 3 0 1 0-3-3a3 3 0 0 0 3 3m0-4a1 1 0 1 1-1 1a1 1 0 0 1 1-1"/><path d="M26 4H6a2 2 0 0 0-2 2v20a2 2 0 0 0 2 2h20a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2m0 22H6v-6l5-5l5.59 5.59a2 2 0 0 0 2.82 0L21 19l5 5Zm0-4.83l-3.59-3.59a2 2 0 0 0-2.82 0L18 19.17l-5.59-5.59a2 2 0 0 0-2.82 0L6 17.17V6h20Z"/></svg>`,
+    'text': `<svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 24 24"><path d="M5 21q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h10l6 6v10q0 .825-.587 1.413T19 21zm2-4h10v-2H7zm0-4h10v-2H7zm0-4h7V7H7z"/></svg>`,
+    'unknown': `<svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 24 24"><path d="M17 22q-2.075 0-3.537-1.463T12 17t1.463-3.537T17 12t3.538 1.463T22 17t-1.463 3.538T17 22m0-2q.275 0 .463-.187t.187-.463t-.187-.462T17 18.7t-.462.188t-.188.462t.188.463T17 20m-.45-1.9h.9v-.25q0-.275.15-.488t.35-.412q.35-.3.55-.575t.2-.775q0-.725-.475-1.162T17 14q-.575 0-1.037.338t-.663.912l.8.35q.075-.3.313-.525T17 14.85q.375 0 .588.188t.212.562q0 .275-.15.463t-.35.387q-.15.15-.312.3t-.288.35q-.075.15-.112.3t-.038.35zM12 9h5l-5-5l5 5l-5-5zM5 22q-.825 0-1.412-.587T3 20V4q0-.825.588-1.412T5 2h8l6 6v2.3q-.5-.15-1-.225T17 10q-1.425 0-2.687.538T12.1 12H7v2h3.675q-.225.475-.375.975T10.075 16H7v2h3.075q.175 1.125.7 2.163T12.125 22z"/></svg>`
+};
+
+window.fileTypeOrder = ['dir', 'package', 'video', 'audio', 'image', 'text', 'unknown'];
+
+window.fileTypeOrderMap = {
+    'dir': 0,
+    'package': 1,
+    'video': 2,
+    'audio': 3,
+    'image': 4,
+    'text': 5,
+    'unknown': 6
+}
+
 window.ajax = {
     getJson: function (url, callback) {
         const xhr = new XMLHttpRequest();
@@ -58,4 +141,25 @@ function connectPath(path1, path2) {
         path2 = path2.slice(1, path2.length);
     }
     return path1 + '/' + path2;
+}
+
+function getFileTypeFromExtension(filename) {
+    const i = filename.lastIndexOf('.');
+    if (i === -1) {
+        return 'unknown';
+    }
+    const extensionName = filename.slice(i + 1);
+    const fileType = extensionTypeMap[extensionName];
+
+    return fileType === undefined ? 'unknown' : fileType;
+}
+
+function getHumanFileSize(size) {
+    const unitArray = ['B', 'KB', 'MB', 'GB', 'TB'];
+    let i = 0;
+    while (size >= 1024) {
+        size = size / 1024;
+        i = i + 1;
+    }
+    return size.toFixed(2) + ' ' + unitArray[i];
 }
--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,15 +1,43 @@
 (() => {
 
+    let fatherDir;
+    let pageFiles;
+
     main();
 
     function main() {
-        ajax.getJson(`/api/index${location.search}`, (response) => {
-            renderContent(response['father'], response['files']);
-        });
+        document.getElementById('sort-btn').addEventListener('click', showSortDialog);
+        document.querySelector('#sort-dialog .select-widget select.by').addEventListener('change', sortBySelectEvent);
+        document.querySelector('#sort-dialog .btn-group .submit').addEventListener('click', submitSortSelectEvent);
+        document.querySelector('#sort-dialog .btn-group .close').addEventListener('click', closeSortDialog);
+
+        // ajax.getJson(`/api/index${location.search}`, (response) => {
+        //     renderContent(response['father'], response['files']);
+        // });
+        const response = {
+            "father": "/root/file-server-android",
+            "files": [
+                {"filename": "ab.mp3", "is_dir": false, "size": 24576},
+                {"filename": "frontend", "is_dir": true, "size": 0},
+                {"filename": "database", "is_dir": true, "size": 2049},
+                {"filename": ".env", "is_dir": false, "size": 59},
+                {"filename": "filename", "is_dir": false, "size": 59},
+                {"filename": "filename.txt", "is_dir": false, "size": 100},
+                {"filename": "filename.zip", "is_dir": false, "size": 2457622},
+                {"filename": "filename.mp4", "is_dir": false, "size": 245761024},
+                {"filename": "655.jpg", "is_dir": false, "size": 24576}
+            ]
+        }
+
+        fatherDir = response['father'];
+        pageFiles = response['files'];
+        pageFiles = sortFiles(pageFiles);
+
+        renderContent(fatherDir, pageFiles);
     }
 
-    function renderContent(fatherDir, filenames) {
-        if (filenames.length === 0) {
+    function renderContent(fatherDir, files) {
+        if (files.length === 0) {
             return undefined;
         }
 
@@ -20,32 +48,175 @@
         if (visibleDir === undefined && path === undefined) {
             // 场景1
             hrefTemplate = (filename) => { return `/?visible_dir=${filename}` };
-            console.log('场景1');
         } else if (visibleDir !== undefined && path === undefined) {
             // 场景2
             hrefTemplate = (filename) => { return `/?visible_dir=${fatherDir}&path=${filename}` };
-            console.log('场景2');
         } else if (visibleDir !== undefined && path !== undefined) {
             // 场景3
             hrefTemplate = (filename) => { return `/?visible_dir=${visibleDir}&path=${connectPath(path, filename)}` };
-            console.log('场景3');
         } else {
-            console.log('场景4');
             return undefined;
         }
 
         const fileList = document.getElementById('file_list');
-        fileList.querySelector('.empty-hint-text').remove();
-        for (let i = 0; i < filenames.length; i++) {
-            let filename = filenames[i];
-            let href = hrefTemplate(filename);
+
+        fileList.querySelector('.empty-hint-text').classList.add('hidden');
+        fileList.querySelectorAll('.file').forEach((f) => {
+            f.remove();
+        });
+
+        for (let i = 0; i < files.length; i++) {
+            let info = files[i];
+            let href = hrefTemplate(info['filename']);
+            let fileType = info['is_dir'] ? 'dir' : getFileTypeFromExtension(info['filename']);
+            let fileSize = info['is_dir'] ? '' : getHumanFileSize(info['size']);
             let html = `
                 <a class="file" href="${href}">
-                    <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="146.43" height="128" viewBox="0 0 1025 896"><path d="M960.232 896h-896q-26 0-45-19t-19-45V256h1024v576q0 26-19 45t-45 19m-960-704V64q0-27 18.5-45.5T64.232 0h384q26 0 45 18.5t19 45.5t18.5 45.5t45.5 18.5h384q26 0 45 19t19 45z"/></svg>
-                    <div class="filename">${filename}</div>
+                    ${fileTypeIconMap[fileType]}
+                    <div class="filename">${info['filename']}</div>
+                    <div class="filesize">${fileSize}</div>
                 </a>`;
+            let ele = htmlToElement(html);
+            ele.querySelector('svg').setAttribute('class', `icon icon-${fileType}`);
 
-            fileList.appendChild(htmlToElement(html));
+            fileList.appendChild(ele);
         }
+    }
+
+    function sortFilesByType(files, reversed) {
+        const filesObject = {};
+        for (let i = 0; i < fileTypeOrder.length; i++) {
+            filesObject[fileTypeOrder[i]] = [];
+        }
+        for (let i = 0; i < files.length; i++) {
+            let info = files[i];
+            let k = info['is_dir'] ? 'dir' : getFileTypeFromExtension(info['filename']);
+            filesObject[k].push(info);
+        }
+        for (let i = 0; i < fileTypeOrder.length; i++) {
+            let k = fileTypeOrder[i];
+            filesObject[k] = sortFilesByName(filesObject[k], false);
+        }
+
+        const mergeKeys = [];
+        for (let i = 0; i < fileTypeOrder.length; i++) {
+            mergeKeys.push(fileTypeOrder[i]);
+        }
+        if (reversed === true) {
+            mergeKeys.reverse();
+        }
+
+        let result = [];
+        for (let i = 0; i < mergeKeys.length; i++) {
+            result = result.concat(filesObject[mergeKeys[i]]);
+        }
+
+        return result;
+    }
+
+    function sortFilesByName(files, reversed) {
+        files.sort((a, b) => {
+            const fn1 = a['filename'];
+            const fn2 = b['filename'];
+            const length = fn1.length > fn2.length ? fn2.length : fn1.length;
+            for (let i = 0; i < length; i++) {
+                let c1 = fn1.charCodeAt(i);
+                let c2 = fn2.charCodeAt(i);
+                if (c1 === c2) {
+                    continue;
+                }
+                return c1 - c2;
+            }
+
+            if (fn1.length > fn2.length) {
+                return 1;
+            } else if (fn1.length === fn2.length) {
+                return 0;
+            } else {
+                return -1;
+            }
+        });
+        if (reversed === true) {
+            files.reverse();
+        }
+        return files;
+    }
+
+    function sortFilesBySize(files, reversed) {
+        let isDirFiles = [];
+        const notDirFiles = [];
+        for (let i = 0; i < files.length; i++) {
+            if (files[i]['is_dir']) {
+                isDirFiles.push(files[i]);
+            } else {
+                notDirFiles.push(files[i]);
+            }
+        }
+
+        isDirFiles = sortFilesByName(isDirFiles, false);
+
+        notDirFiles.sort((a, b) => {
+            return a['size'] - b['size'];
+        });
+        if (reversed === true) {
+            notDirFiles.reverse();
+        }
+
+        return isDirFiles.concat(notDirFiles);
+    }
+
+    function sortFiles(files, by, reversed) {
+        if (by !== undefined && by !== 'type' && by !== 'name' && by !== 'size') {
+            throw 'by参数不可用'
+        }
+        if (by === undefined) {
+            by = localStorage.getItem('sort_files_by');
+            by = by === null ? 'type' : by;
+        } else {
+            localStorage.setItem('sort_files_by', by);
+        }
+
+        if (reversed === undefined) {
+            reversed = localStorage.getItem('sort_files_reversed') === 'true';
+        } else {
+            localStorage.setItem('sort_files_reversed', String(reversed));
+        }
+
+        if (by === 'type') {
+            return sortFilesByType(files, reversed);
+        } else if (by === 'name') {
+            return sortFilesByName(files, reversed);
+        } else if (by === 'size') {
+            return sortFilesBySize(files, reversed);
+        }
+        return files;
+    }
+
+    function showSortDialog() {
+        document.getElementById('overlay').classList.remove('hidden');
+        document.getElementById('sort-dialog').classList.remove('hidden');
+    }
+
+    function closeSortDialog() {
+        document.getElementById('overlay').classList.add('hidden');
+        document.getElementById('sort-dialog').classList.add('hidden');
+    }
+
+    function sortBySelectEvent(e) {
+        const select = e.target;
+        const option = select.options[select.selectedIndex];
+        const control = option.getAttribute('data-control');
+        document.querySelector('#sort-dialog .select-widget select.reversed:not(.hidden)').classList.add('hidden');
+        document.querySelector(`#sort-dialog .select-widget select.reversed[data-control="${control}"]`).classList.remove('hidden');
+    }
+
+    function submitSortSelectEvent(e) {
+        const by = document.querySelector('#sort-dialog .select-widget select.by').value;
+        let reversed = document.querySelector('#sort-dialog .select-widget select.reversed:not(.hidden)').value === '1';
+        console.log(by, reversed);
+
+        renderContent(fatherDir, sortFiles(pageFiles, by, reversed))
+
+        closeSortDialog();
     }
 })();
