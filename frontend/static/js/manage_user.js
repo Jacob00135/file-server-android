c0107c4d37d5e2cfde231367aee2a5bde2fb9527
XieJiyue <2428207444@qq.com>
2024-09-29 02:00:59 +0800

    添加管理用户界面

--- frontend/static/js/manage_user.js
+++ frontend/static/js/manage_user.js
@@ -22,8 +22,13 @@
         for (let i = 0; i < users.length; i++) {
             let user = users[i];
             let html = `<div class="row">
-                <div class="item">${user['username']}</div>
-                <div class="item">${permissionMap[user['permission']]}</div>
+                <div class="item username">${user['username']}</div>
+                <div class="item permission">
+                    <div class="text">${permissionMap[user['permission']]}</div>
+                </div>
+                <button class="item delete-btn" type="button">
+                    <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 24 24"><path d="M19 4h-3.5l-1-1h-5l-1 1H5v2h14M6 19a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6z"/></svg>
+                </button>
             </div>`;
 
             mainElement.appendChild(htmlToElement(html));
