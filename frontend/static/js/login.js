e785423360f4a897b522697b9a5ad211330e5e6b
XieJiyue <2428207444@qq.com>
2024-09-28 11:05:29 +0800

    登录页面使用ajax交互

--- frontend/static/js/login.js
+++ frontend/static/js/login.js
@@ -1,3 +1,29 @@
 (() => {
-    
+
+    main();
+
+    function main() {
+        document.querySelector('#login-widget form').addEventListener('submit', loginFormSubmitEvent);
+    }
+
+    function loginFormSubmitEvent(e) {
+        e.preventDefault();
+
+        const form = document.querySelector('#login-widget form');
+        const data = {
+            'username': form['username'].value,
+            'password': form['password'].value
+        };
+        ajax.postJson('/api/login', data, (response) => {
+            if (response['status'] === 1) {
+                location.assign('/');
+                return undefined;
+            }
+
+            const errorMessageElement = document.getElementById('error-message');
+            errorMessageElement.classList.remove('hidden');
+            errorMessageElement.textContent = response['message'];
+        });
+    }
+
 })();
